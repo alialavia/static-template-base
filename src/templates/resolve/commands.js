e0ded3836415a718a2508ef95e993200dfb27269
Ali Alavi <alialavia@gmail.com>
2018-06-30 15:45:04 -0400

    src added

---
+++ src/templates/resolve/commands.js
@@ -0,0 +1,12 @@
+var self = module.exports = {
+    detail_summary: (title, desc) =>
+        `  <details open>
+    <summary>${title}</summary>
+    <p>${desc}</p>
+  </details>`,
+    edu: (year, school, title, desc) => self.detail_summary(`${year} - ${school} - ${title}`, desc),
+    work: (year, company, title, desc) => self.detail_summary(`${year} - ${company} - ${title}`, self.ul(...desc)),
+    li: (item) => `<li>${item}</li>`,
+    ul: (...items) => `<ul>${items.map(self.li).join('')}</ul>`
+
+};
